2a7ac72810222dafa7182f92b7f12edb9de3c8d1
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-06-19 13:17:21 +0530

    solidity

---
+++ Project/FundMe7Prac.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract giveMeFund{
+
+uint256 public constant Minimum_USD = 5 * 1e18;
+
+address[] public UsersAddress;
+mapping(address funders => uint256 NetAmountPaid) public AddToAmnt;
+
+function fund() public payable{
+    require(AmntInUsd(msg.value)>Minimum_USD , "Work hard and pay me more ETH brooo");
+    UsersAddress.push(msg.sender);
+    AddToAmnt[msg.sender] = AddToAmnt[msg.sender] + msg.value;
+
+}
+
+function getPrice() public view returns(uint256){
+
+(,int256 price,,,) = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419).latestRoundData();
+
+return uint256(price*1e10);
+}
+
+function AmntInUsd(uint256 ethAmount) public view returns(uint256){
+    uint256 singleEthPriceInUsd = getPrice();
+    uint TotalPriceInUsd = (singleEthPriceInUsd * ethAmount)/1e18;
+
+    return TotalPriceInUsd;
+
+}
+
+function withdraw() public{
+    for(uint i = 0 ;i<UsersAddress.length ; i++){
+        address funders = UsersAddress[i];
+        AddToAmnt[funders] = 0;
+    }
+
+    UsersAddress = new address[](0);
+
+    // transfer
+
+    payable(msg.sender).transfer(address(this).balance);
+
+
+    // send
+
+    bool sendSuccess = payable(msg.sender).send(address(this).balance);
+    require(sendSuccess , "Trans Failed");
+
+    // call
+
+   (bool sendSucc ,) = payable(msg.sender).call{value:address(this).balance}("");
+   require(sendSucc , "Trans Failed");
+
+}
+}
