c442c4ce5b425b8e8e7ef456aad85a8deed00c2e
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-07-17 12:22:18 +0530

    TIMEVAULT

--- TimeLockedVault.sol
+++ TimeLockedVault.sol
@@ -15,10 +15,15 @@ address[] public funders;
 
 mapping(address donaters => uint256 amntFunded) public addToAmnt;
 
-address public owner;
+address public immutable owner;
+
+uint256 public immutable startTime;
+
+// immutable se gas kam use hoti
 
 constructor(){
     owner = msg.sender;
+    startTime = block.timestamp;
 }
 
     function fund() public payable{
@@ -43,9 +48,9 @@ addToAmnt[msg.sender] += msg.value;
 
 
 
- function withdraw() public{
+ function withdraw() public onlyOwner{
 
-    require(msg.sender == owner , "you aint an owner broo");
+  
 
     for(uint256 i = 0 ; i<funders.length;i++){
         addToAmnt[funders[i]] = 0;
@@ -68,4 +73,51 @@ require(Succ,"withdraw failed");
 // i forgot to add the external and payable..
 // the reason why we use external is coz we will be calling these function outside of the contract all the time 
 // payable means we can pay this contract which is what we are doing rnn
+
+
+
+
+ modifier onlyOwner(){
+   require(msg.sender == owner , "you aint an owner broo");
+   require(block.timestamp >= startTime + 7 days,"have patience broo");
+   _;
+ }
+
+
+
 }
+
+/*
+NEW LEARNING
+
+block.timestamp always gives you the current time at the exact moment the block containing your transaction is processed.
+
+Just remember: it doesn't give you a date format like July 17, 2026. Instead, it gives it to you as a total number of seconds (Unix time) that has passed since Jan 1 1970
+
+So if you check it right now, it will look like a massive number (e.g., 1784289346). When a transaction runs, that massive number is what Solidity works with.
+
+It returns a uint256 value measured in seconds, not milliseconds
+
+Read-Only: You cannot assign a value to it (e.g., block.timestamp = 0 will throw a compiler error).
+
+
+NATIVE TIME UNITS
+
+
+
+
+seconds (e.g., 1 seconds = 1)
+
+minutes (e.g., 1 minutes = 60)
+
+hours (e.g., 1 hours = 3600)
+
+days (e.g., 1 days = 86400)
+
+weeks (e.g., 1 weeks = 604800)
+
+Note: The older years suffix has been deprecated and removed because leap years change the number of seconds in a year.
+
+
+
+*/
